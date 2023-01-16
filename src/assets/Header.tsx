206306fa1a83c749feb273c507830d7ad7a5abcc
Merryl Arokiasamy <merrylarok@gmail.com>
2023-01-15 21:43:30 -0700

    Created basic layout with api integration

--- src/assets/Header.tsx
+++ src/assets/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <nav className="header">
             <img className="header--logo" src={logo} alt="headerLogo" />
-            <h1><a href="#">Memenator</a></h1>
+            <h1><a href="#">LeetMemes</a></h1>
             <h2><a href="https://imgflip.com/api" target="_blank">API Documentation</a></h2>
 
         </nav>)
--- src/assets/Meme.tsx
+++ src/assets/Meme.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
 export default function Meme() {
+
     return (
-        <form action="" className="form">
-            <input className="form--textElement" type="text" placeholder="First Sentence" />
-            <input className="form--textElement" type="text" placeholder="Second Sentence" />
-            <button className="form--submit">Generate random meme</button>
-        </form>
+
+        <section className="main">
+            <h2>A Breakthrough Approach to Mastering the Art of Creating Hilarious Memes</h2>
+            <p>LeetMemes is the best platform to practice making memes, master internet speak and crack the code to internet points.</p>
+            <form action="" className="form">
+                <input className="form--textElement" type="text" placeholder="Top Sentence" />
+                <input className="form--textElement" type="text" placeholder="Bottom Sentence" />
+                <button className="form--submit">Generate random meme</button>
+            </form>
+        </section>
+
     );
 }
